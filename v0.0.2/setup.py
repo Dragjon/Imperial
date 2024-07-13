48af915f9210d82fce2270222bb3366d3da68b7d
Dragjon <140328303+Dragjon@users.noreply.github.com>
2024-07-13 21:43:32 +0800

    v0.0.2: Polonium rewrite in python

---
+++ v0.0.2/setup.py
@@ -0,0 +1,15 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Define the executable
+executables = [
+    Executable('imperious.py', target_name='imperious.exe')
+]
+
+# Setup configuration
+setup(
+    name='Imperious',
+    version='1.0',
+    description='Imperious Chess',
+    executables=executables
+)
